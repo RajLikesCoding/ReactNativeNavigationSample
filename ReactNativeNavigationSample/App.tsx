3bab4c1a55d517c0fc1f1b303208146aa8fd25da
Rajarajan <rajlikescoding@gmail.com>
2024-10-19 21:27:34 +0530

    Start Project

---
+++ ReactNativeNavigationSample/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View>
+        <Text>Stack Navigation</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
